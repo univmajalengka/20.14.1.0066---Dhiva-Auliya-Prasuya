99e371124f918f08fa280fbcc444523ea8fe8a0c
Dhivaauliya <74223954+Dhivaauliya@users.noreply.github.com>
2021-04-22 15:57:15 +0700

    Add files via upload

--- Ujian/Soal_A.pas
+++ Ujian/Soal_A.pas
@@ -1,40 +1,32 @@
-{Nama : Dhiva Auliya Prasuya}
-{NPM : 20.14.1.0066}
-{Kelompok : 5}
-{Kode Soal : A, B, C, D}
+{NAMA         : SITI ADE IDA ROSIDA}
+{NPM          : 20.14.1.0023}
+{KELOMPOK     : Kelompok 2}
+{KODE SOAL    : A, B, C, D}
 
 program Soal_A;
 uses crt;
 
-procedure biodata;
 var
-nama , npm, alamat, wa : string;
-jenis_kelamin : char;
-
-begin
-writeln('Program Biodata Mahasiswa');
-writeln('===============================================');
-write('Masukan Nama mahasiswa            : '); readln(nama);
-write('Masukan NPM  mahasiswa            : '); readln(npm);
-write('Masukan Jenis Kelamin  (L/P)      : '); readln(jenis_kelamin);
-write('Masukan Alamat                    : '); readln(alamat);
-write('Masukan Kontak Anda               : '); readln(wa);
-
-writeln('Nama Anda Adalah ', nama);
-writeln('NPM ', npm);
-writeln('Dengan Jenis Kelamin ', jenis_kelamin);
-writeln('Dengan Alamat ', alamat);
-writeln('Kontak Anda ', wa);
-
-
-end;
+nama,npm,jenis_kelamin,alamat,kontak_whatsapp  :string;
 
 begin
 clrscr;
-biodata;
+writeln('Program Biodata Sederhana');
+writeln('=========================');
+writeln('Masukan Biodata anda');
+writeln;
+write('Masukan Nama :');readln(nama);
+write('Masukan NPM :');readln(npm);
+write('Masukan Jenis Kelamin :');readln(jenis_kelamin);
+write('Masukan Alamat :');readln(Alamat);
+write('Masukan Nomor kontak whatsap :');readln(kontak_whatsapp );
+writeln;
+writeln('Biodata Anda');
+writeln('======================');
+writeln;
+writeln('Nama anda adalah ', nama, '  dengan NPM ',npm);
+writeln(' Anda berjenis kelamin ',jenis_kelamin,' dan beralamat di ',alamat);
+writeln(' Nomor kontak yang bisa di hubungi ',kontak_whatsapp );
 
 readln;
-end.
-begin
-
 end.
