06b963aa8d391cfbd8a2a9e2386cd8bb43e897b4
Dhivaauliya <74223954+Dhivaauliya@users.noreply.github.com>
2021-04-22 16:02:36 +0700

    Add files via upload

---
+++ Ujian/Soal_A.pas
@@ -0,0 +1,32 @@
+{NAMA         : DHIVA AULIYA PRASUYA}
+{NPM          : 20.14.1.0066}
+{KELOMPOK     : Kelompok 5}
+{KODE SOAL    : A, B, C, D}
+
+program Soal_A;
+uses crt;
+
+var
+nama,npm,jenis_kelamin,alamat,kontak_whatsapp  :string;
+
+begin
+clrscr;
+writeln('Program Biodata Sederhana');
+writeln('=========================');
+writeln('Masukan Biodata anda');
+writeln;
+write('Masukan Nama :');readln(nama);
+write('Masukan NPM :');readln(npm);
+write('Masukan Jenis Kelamin :');readln(jenis_kelamin);
+write('Masukan Alamat :');readln(Alamat);
+write('Masukan Nomor kontak whatsap :');readln(kontak_whatsapp );
+writeln;
+writeln('Biodata Anda');
+writeln('======================');
+writeln;
+writeln('Nama anda adalah ', nama, '  dengan NPM ',npm);
+writeln(' Anda berjenis kelamin ',jenis_kelamin,' dan beralamat di ',alamat);
+writeln(' Nomor kontak yang bisa di hubungi ',kontak_whatsapp );
+
+readln;
+end.
